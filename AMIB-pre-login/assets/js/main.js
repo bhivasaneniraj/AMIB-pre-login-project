ba13cdd77ac9ec32a98c27e8c8a879478a87b32d
Niraj.Bhivasane <Niraj.Bhivasane@almullaexchange.com>
2023-05-09 18:15:20 +0530

    changes done by shubhadip sir

--- AMIB-pre-login/assets/js/main.js
+++ AMIB-pre-login/assets/js/main.js
@@ -37,6 +37,21 @@ $(function () {
     })
   })
 
+  var scrollLink = $(".page-scroll")
+  // Active link switching
+  $(window).scroll(function () {
+    var scrollbarLocation = $(this).scrollTop()
+
+    scrollLink.each(function () {
+      var sectionOffset = $(this.hash).offset().top - 73
+
+      if (sectionOffset <= scrollbarLocation) {
+        $(this).parent().addClass("active")
+        $(this).parent().siblings().removeClass("active")
+      }
+    })
+  })
+
   //for request a call back
 
   $("#button").click(function () {
